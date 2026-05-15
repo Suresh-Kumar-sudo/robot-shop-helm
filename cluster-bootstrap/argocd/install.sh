40576a4ae72e78375c7edf6add8063bb2995f86f
Suresh-Kumar-sudo <sureshkumarnadar1701@gmail.com>
2026-05-15 09:06:57 +0530

    eks mode on

--- cluster-bootstrap/argocd/install.sh
+++ cluster-bootstrap/argocd/install.sh
@@ -4,4 +4,4 @@ kubectl create namespace argocd
 
 kubectl apply -n argocd -f install.yaml
 
-kubectl apply -f applications/robot-shop-app.yaml
+kubectl apply -f applications/robot-shop-app.yamlkubectl describe application robot-shop -n argocd
